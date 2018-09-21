d5212bbddc0d908ec4f828d3e83faab36d26db85
Rita Raher <ritaraher@gmail.com>
2018-09-21 08:40:56 +0100

    for loops, raneg, f strings

--- Wk1_thebasics.py
+++ Wk1_thebasics.py
@@ -28,4 +28,25 @@ b = 20
 while b > 0:
     a, b = b, a % b
 print(a)
-    
+
+# for loops
+l = [0,1,2,3,4,5,6,7,8,9]
+
+for i in l:
+    print(i, i*i)
+    print(" ")
+
+print("That's the whole list!")
+
+# range
+for i in range(20):
+    print(i, i*i)
+
+print("That's the whole list!")
+
+
+# f strings
+for i in range(20):
+    print(f"{i:2} {i*i:4}")
+
+print("That's the whole list!")
