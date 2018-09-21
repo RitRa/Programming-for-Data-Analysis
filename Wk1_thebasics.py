b218dc587a9116b5a9afcd5b7aafc097fb91be28
Rita Raher <ritaraher@gmail.com>
2018-09-21 08:08:21 +0100

    update to Is it Tuesday

--- Wk1_thebasics.py
+++ Wk1_thebasics.py
@@ -3,8 +3,14 @@ import datetime as d
 today = d.datetime.today()
 dayofweek = today.weekday()
 
+print("Let's check if it's Tuesday?")
 
 if dayofweek == 1:
     print("It's Tuesday!")
+elif dayofweek == 0:
+    print("It's not Tuesday tomorrow!")
+
 else:
     print("Unfortunately, It's not Tuesday")
+
+print("Thanks for checking if it's Tuesday!")
