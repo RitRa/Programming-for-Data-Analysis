6793a556dcda5eb7d5583b2825e3a058bcf9eff3
Rita Raher <ritaraher@gmail.com>
2018-09-28 19:13:47 +0100

    matplotlib update

--- Matplotlib/2dplot.py
+++ Matplotlib/2dplot.py
@@ -1,10 +1,36 @@
 # Rita Raher 28/09/2018
 # matplotlib examples
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 plt.plot([1, 2, 3, 4])
 plt.ylabel('some numbers')
 
 plt.show()
 
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+plt.ylabel('some numbers')
+
+plt.show()
+
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'b.')
+plt.ylabel('some numbers')
+
+plt.show()
+
+
+x = np.arange(0.0, 10.0, 0.01)
+y = 3.0 * x + 1.0
+
+noise = np.random.normal(0.0, 1.0, len(x))
+
+plt.plot(x, y +noise, 'r.')
+plt.plot(x, y, 'b-')
+
+plt.show()
+
+
+plt.plot(x, y +noise, 'c.')
+plt.plot(x, y, 'g-')
+
+plt.show()
