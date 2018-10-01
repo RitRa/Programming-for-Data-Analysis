a92b398224aefb03c8a5e3e38e1f2d983a0ef94d
Rita Raher <ritaraher@gmail.com>
2018-10-01 21:17:55 +0100

    title and legends

--- Matplotlib/titleslegends.py
+++ Matplotlib/titleslegends.py
@@ -6,7 +6,7 @@ import numpy as np
 x = np.arange(0.0, 10.0, 0.01)
 y = 3.0 * x + 1.0
 
-noise = numpy.random.normal(0.0, 1.0, len(x))
+noise = np.random.normal(0.0, 1.0, len(x))
 
 plt.plot(x, y + noise, 'r.')
 plt.plot(x, y, 'b-')
