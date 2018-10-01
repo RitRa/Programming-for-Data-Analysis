9c763aad234b27af7b6b5e65bcd7b1cd77d0be6c
Rita Raher <ritaraher@gmail.com>
2018-10-01 21:47:31 +0100

    plots update

--- Matplotlib/titleslegends.py
+++ Matplotlib/titleslegends.py
@@ -1,5 +1,3 @@
-# Rita Raher 28/09/2018
-# matplotlib examples
 import matplotlib.pyplot as plt
 import numpy as np
 
---
+++ Matplotlib/twoplotssidebyside.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.subplot(1, 2, 1)
+x = np.random.normal(0.0, 1.0, 1000)
+
+plt.hist(x)
+
+plt.subplot(1, 2, 2)
+x = np.random.uniform(0.0, 1.0, 1000)
+
+plt.hist(x)
+
+plt.show()
